f1dcada88e11412a48f9c7ec91cd6a74dafa3928
meiousei9 <cameronavvampato@gmail.com>
2022-03-23 21:39:51 -0400

    buggy version, notably subtraction and negative numbers tend to cause problems. will fix soon. saving work for now

--- main.js
+++ main.js
@@ -1,14 +1,148 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+
+//REGEXS
+const REGEX_NUM_OP_NUM = /\-?\d+[\+\-\*x\/]\-?\d+/; //regex for (+/-) number, then +, -, */x, or /, then (+/-) number
+const REGEX_OPERATORS = /([\+\-\*x\/])/; //regex for operators, w/ catching brackets () (used for .split)
+const REGEX_NEGATIVE_NUM = /\-\d+/;
+
+//SELECTORS
+const inputButtons = document.querySelectorAll('.numInput, .opInput'); //NOTE: PERIOD "." IS INCLUDED UNDER ".numInput"
+const clearButton = document.querySelector('#clearButton');
+const equalButton = document.querySelector('#equalButton');
+
+const mainDisplay = document.querySelector('#mainDisplay');
+const historyDisplay = document.querySelector('#historyDisplay');
+
 main();
 
-//global variables
-
-
 function main() {
-
+    //clears display if previously used
+    clearDisplay();
+    //basically sets up the whole calculator
     makeButtonsClickable();
+}
 
-    let userInput = window.prompt("Enter a simple math equation: ");
-    operate(userInput);
+///////////////
+// FUNCTIONS //
+///////////////
+
+//makes buttons clickable
+function makeButtonsClickable() {
+    //makes NUMBERS AND OPERATORS buttons do things    
+    inputButtons.forEach(inputButton => {
+        inputButton.addEventListener('click', () => displayOnMainDisplay(inputButton.textContent));
+    });
+    console.log("nofun");
+
+    //makes CLEAR button do things
+    clearButton.addEventListener('click', () => clearDisplay());
+
+    //makes ENTER button (=) do things
+    equalButton.addEventListener('click', () => checkIfEquation(mainDisplay.textContent));
+}
+
+//makes input display on mainDisplay, then store that variable into 
+function displayOnMainDisplay(input) {
+    if (typeof input === "string")
+        input = input.trim(); //gets rid of extra space, idk why it's there
+    
+    if ( !isNaN(input) || input === "." ) {
+        //for numbers or decimal point
+        if (mainDisplay.textContent === "0") {
+            mainDisplay.textContent = input;
+        } else {
+            mainDisplay.textContent += input;    
+        }
+    } else if (input === "-" && mainDisplay.textContent === "0") {
+        //dealing with making 0 mainDisplay negative
+        mainDisplay.textContent = input;
+    } else {
+        //for operators 
+        if (REGEX_OPERATORS.test(mainDisplay.textContent)) {
+            //if there's an operator prior in the equation
+            checkIfEquation(mainDisplay.textContent);
+            
+            if (REGEX_NEGATIVE_NUM.test(mainDisplay.textContent)) {
+                //if it fails the equation test and there's a negative number
+                mainDisplay.textContent += " " + input + " ";
+            }
+            
+        } else {
+            //simple, no operators prior to this input
+            mainDisplay.textContent += " " + input + " ";
+        }
+    }
+}
+
+//displays equation performed in historyDisplay
+function displayOnHistoryDisplay(equation) {
+    historyDisplay.textContent = equation;
+}
+
+//clear display and equation (used by clear button)
+function clearDisplay() {    
+    if (mainDisplay.textContent !== "0") {
+        //if there are numbers in mainDisplay
+        mainDisplay.textContent = "0";
+    } else {
+        //code to reset operation too
+    }
+}
+
+function checkIfEquation (equation) {
+    //removes all whitespace
+    let equationNoSpace = equation.replace(/\s/g,"");
+    
+    //returns if not an equation formatted properly
+    if (!REGEX_NUM_OP_NUM.test(equationNoSpace)) {
+        console.log("not an equation");
+        return;
+    }
+
+    //splits the equation into num1, operator, num2
+    const splitOperation = equation.split(REGEX_OPERATORS);
+    console.log(splitOperation);
+    
+    const num1 = Number(splitOperation[0]);
+    const operator = splitOperation[1];
+    const num2 = Number(splitOperation[2]);
+
+    //call operate
+    let result = operate (num1, operator, num2);
+
+    displayOnHistoryDisplay(equation);
+    clearDisplay();
+    displayOnMainDisplay(result);
+}
+
+// determines which math function to use
+// operate works when pass in string
+function operate (num1, operator, num2) {
+
+    let result = null;
+    //switch determines which operation to use
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+        case "x":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            console.log("invalid operator");
+            break;
+    } 
+
+    return result;
 }
 
 // 4 BASIC MATH FUNCTIONS
@@ -26,112 +160,4 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-}
-
-///////////////
-// FUNCTIONS //
-///////////////
-
-// determines which math function to use
-// operate works when pass in string
-function operate (userInput) {
-
-
-    /////////////MAKE THIS ITS OWN FUNCTION - "CHECKIFEQUATION"///////////
-    //regexs
-    const REGEX_NUM_OP_NUM = /\d+[\+\-\*x\/]\d+/; //regex for number, then +, -, */x, or /, then number
-    const REGEX_OPERATORS = /([\+\-\*x\/])/; //regex for operators, w/ catching brackets () (used for .split)
-
-    //removes all whitespace
-    userInput = userInput.replace(/\s/g,"");
-
-    
-    //rejects if not an equation formatted properly
-    if (!REGEX_NUM_OP_NUM.test(userInput)) {
-        console.log("bad equation");
-        return;
-    }
-
-
-    //splits the equation into num1, operator, num2
-    const splitOperation = userInput.split(REGEX_OPERATORS);
-    console.log(splitOperation);
-  
-    const num1 = Number(splitOperation[0]);
-    const operator = splitOperation[1];
-    const num2 = Number(splitOperation[2]);
-
-
-    /////////////////////////////////////////////
-
-
-    //switch determines which operation to use
-    // prob need to return some variable later, for use in equations like 1 + 2 - 3 -> need to separate to 1 + 2, then 3 - 3
-    switch (operator) {
-        case "+":
-            console.log(add(num1, num2));
-            break;
-        case "-":
-            console.log(sub(num1, num2));
-            break;
-        case "*":
-        case "x":
-            console.log(multiply(num1, num2));
-            break;
-        case "/":
-            console.log(divide(num1, num2));
-            break;
-        default:
-            console.log("invalid operator");
-            break;
-    } 
-}
-
-//makes buttons clickable
-function makeButtonsClickable() {
-    //makes NUMBERS AND OPERATORS buttons do things
-    const inputButtons = document.querySelectorAll('.inputButtons > button');
-    
-    inputButtons.forEach(inputButton => {
-        inputButton.addEventListener('click', () => displayOnDisplay(inputButton.textContent));
-    });
-
-    //makes CLEAR button do things
-    const clearButton = document.querySelector('#clearButton');
-
-    clearButton.addEventListener('click', () => clearDisplay());
-
-}
-
-//makes input display on display, then store that variable into 
-function displayOnDisplay(input) {
-    input = input.trim(); //gets rid of extra space, idk why it's there
-
-    const display = document.querySelector('#display');
-    
-    if ( !isNaN(input) ) {
-    //for numbers 
-        if (display.textContent === "0") {
-            display.textContent = input;
-        } else {
-            display.textContent += input;    
-        }
-    } else if (input === ".") {
-    //for decimal
-        display.textContent += input;
-    } else {
-    //for operators 
-        display.textContent += " " + input + " ";
-    }
-}
-
-//clear display and equation (used by clear button)
-function clearDisplay() {
-    const display = document.querySelector('#display');
-    
-    if (display.textContent !== "0") {
-        display.textContent = "0";
-    } else {
-        //code to reset operation too
-    }
 }
